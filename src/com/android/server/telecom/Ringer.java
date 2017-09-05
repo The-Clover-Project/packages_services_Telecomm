c1e5cf7a3d2345cc5b02e9f02982eec828d85a1b
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:35:10 +0800

    Incall vibration options[2/3]
    Squashed commit of the following:
    
    commit fa1f556a275b475eee1a689cf5253e0d364f50e7
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sat Jan 27 04:20:53 2024 +0530
    
        Telecomm: More improvements for incall vibrations
    
        * Apply "Vibrate on connect" for incoming calls as well
        * Add checks in place for vibrator service
        * Create static vibration attributes
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    commit ed9a7c20c68e585b9bec9803202080e38cd0e067
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Mon Jul 31 16:13:54 2023 +0300
    
        Telecomm: Get rid of deprecation in incall vibrations
    
        May also fix this feature for older vibrators
    
        Change-Id: I6cc82c36009d8eb2cef26df20978ffbd8cb217a8
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    commit c36a4312ca4bd6e0eaf6e8f5f7c4e015d95f3d1f
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Tue Sep 5 14:35:10 2017 +0800
    
        Incall vibration options [2/3]
    
        *allow setting vibration when call is connected
        *allow setting vibration when call is disconnected
        *allow setting vibration for call waiting
        *this works with google and aosp dialer :)
    
        Change-Id: I1dcdf20d022df94cf798548f0c3e2d524f8b21ca
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: Iaa5cd11fc3b7878422271f81e1fd917ef895bf87

--- src/com/android/server/telecom/Ringer.java
+++ src/com/android/server/telecom/Ringer.java
@@ -43,6 +43,7 @@ import android.os.UserManager;
 import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.provider.Settings;
 import android.telecom.Log;
 import android.telecom.TelecomManager;
 import android.util.Pair;
@@ -195,6 +196,9 @@ public class Ringer {
 
     private static final VibrationAttributes VIBRATION_ATTRIBUTES =
             new VibrationAttributes.Builder().setUsage(VibrationAttributes.USAGE_RINGTONE).build();
+    private static final VibrationAttributes VIBRATION_INCALL_ATTRIBUTES =
+            new VibrationAttributes.Builder().setUsage(VibrationAttributes.USAGE_ACCESSIBILITY).build();
+    private static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
 
     private static VolumeShaper.Configuration mVolumeShaperConfig;
 
@@ -702,6 +706,12 @@ public class Ringer {
 
         stopRinging();
 
+        if (Settings.System.getInt(
+                mContext.createContextAsUser(call.getAssociatedUser(), 0).getContentResolver(),
+                VIBRATE_ON_CALLWAITING, 0) == 1) {
+            vibrate(200, 300, 500);
+        }
+
         if (mCallWaitingPlayer == null) {
             Log.addEvent(call, LogUtils.Events.START_CALL_WAITING_TONE, reason);
             mCallWaitingCall = call;
@@ -1045,6 +1055,16 @@ public class Ringer {
                 mVibrator.cancel();
                 mIsVibrating = false;
             });
+        }
+    }
+
+    public void vibrate(int v1, int p1, int v2) {
+        if (mVibrator != null && mVibrator.hasVibrator()) {
+            long[] pattern = new long[] {
+                0, v1, p1, v2
+            };
+            mVibrator.vibrate(
+                VibrationEffect.createWaveform(pattern, -1), VIBRATION_INCALL_ATTRIBUTES);
         }
     }
 }
